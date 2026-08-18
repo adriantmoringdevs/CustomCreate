01c8dd87174e7dc024e1633b3c133f03a16fd02b
Adrian Moring <adriantmoringbass@gmail.com>
2026-08-18 17:35:53 -0400

    added dashboard reorder points

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -31,25 +31,27 @@ function Dashboard() {
     fetch("http://localhost:5000/api/materials", {
       headers: { Authorization: `Bearer ${token}` },
     })
-    .then((res) => {
-      if (!res.ok) throw new Error("Failed to load materials:");
-      return res.json()
-    })
-    .then((data) => setMaterials(data))
-    .catch((err) => {
-      console.error("Error loading dashobard materials:", err);
-      setError("Couldn't load materials")
-    })
-    .finally(() => setIsLoading(false));
+      .then((res) => {
+        if (!res.ok) throw new Error("Failed to load materials:");
+        return res.json();
+      })
+      .then((data) => setMaterials(data))
+      .catch((err) => {
+        console.error("Error loading dashobard materials:", err);
+        setError("Couldn't load materials");
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   const activeJobs = jobs.filter(
-    (job) => job.status === "QUOTED" || job.status === "IN_PROGRESS"
+    (job) => job.status === "QUOTED" || job.status === "IN_PROGRESS",
   );
 
-  const lowMaterials = materials.filter((material) => material.low_stock === true)
+  const lowMaterials = materials.filter(
+    (material) => material.low_stock === true,
+  );
 
-  console.log(lowMaterials)
+  console.log(lowMaterials);
   if (isLoading) return <div className="dashboard-container">Loading...</div>;
   if (error) return <div className="dashboard-container">{error}</div>;
 
@@ -69,7 +71,9 @@ function Dashboard() {
             >
               <div className="job-card-header">
                 <span className="job-card-name">{job.name}</span>
-                <span className={`badge badge-${job.status.toLowerCase().replace("_", "-")}`}>
+                <span
+                  className={`badge badge-${job.status.toLowerCase().replace("_", "-")}`}
+                >
                   {job.status.replace("_", " ")}
                 </span>
               </div>
@@ -85,6 +89,22 @@ function Dashboard() {
           ))}
         </div>
       )}
+      <div>
+        {lowMaterials.length !== 0 && (
+          lowMaterials.map((material) => (
+            <div 
+            key={material.id}>
+              Low Material Alerts
+              <div className="low-material-card-body">
+                <p className="low-material-card-name">Name: {material.name}</p>
+                <p className="low-material-sku">SKU #: {material.sku}</p>
+                <p className="low-material-reorder-point">Reorder Point: {material.reorder_point}</p>
+                <p className="low-material-total_quantity">Total Quantity: {material.total_quantity}</p>
+              </div>
+              </div>
+          ))
+        ) }
+      </div>
     </div>
   );
 }
